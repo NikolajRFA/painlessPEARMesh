805198aaa448d12efdbd91720d277d652fa20381
NikolajRFA <nrfadk@gmail.com>
2025-03-18 15:11:31 +0100

    monitor speed

--- src/main.cpp
+++ src/main.cpp
@@ -115,6 +115,7 @@ void sendMessage() {
 
 void receivedCallback(uint32_t from, String & msg) {
   Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  Serial.println(mesh.subConnectionJson());
 }
 
 void newConnectionCallback(uint32_t nodeId) {
