e614af29ce6d6546ab5cd14f1609d4448e9470d7
lthyge <laust.thygesen@gmail.com>
2025-03-26 15:25:40 +0100

    Added getRoodNodeId test

--- test/test_get_root_node_id.cpp
+++ test/test_get_root_node_id.cpp
@@ -1,4 +1,5 @@
 #include "unity.h"
+#include "..\src\meshHelpers.cpp"
 
 void setUp(void)
 {
@@ -10,23 +11,28 @@ void tearDown(void)
     // clean stuff up here
 }
 
-
-void test_function_should_doBlahAndBlah(void) {
-    
-    // test stuff
-  }
+void test_getRootNodeId_should_return_3206773453(void)
+{
+    u_int32_t rootId = getRootNodeId("{\"nodeId\" : 3211386233, \"subs\" : [ {\"nodeId\" : 3211408993, \"subs\" : [ {\"nodeId\" : 3206773453, \"root\" : true, \"subs\" : [ {\"nodeId\" : 3206793885} ]} ]} ]}");
+    TEST_ASSERT_EQUAL_INT32(3206773453, rootId);
+}
 
 int runUnityTests(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_function_should_doBlahAndBlah);
+    RUN_TEST(test_getRootNodeId_should_return_3206773453);
     return UNITY_END();
 }
 
 /**
- * For native dev-platform or for some embedded frameworks
+ * For Arduino framework
  */
-int main(void)
+void setup()
 {
-    return runUnityTests();
+    // Wait ~2 seconds before the Unity test runner
+    // establishes connection with a board Serial interface
+    delay(2000);
+
+    runUnityTests();
 }
+void loop() {}
