ea4f64d32ca4607d41966a3a7f248d1660ce7ac1
lthyge <laust.thygesen@gmail.com>
2025-03-26 12:47:08 +0100

    work in progress

---
+++ test/test_get_root_node_id.cpp
@@ -0,0 +1,32 @@
+#include "unity.h"
+
+void setUp(void)
+{
+    // set stuff up here
+}
+
+void tearDown(void)
+{
+    // clean stuff up here
+}
+
+
+void test_function_should_doBlahAndBlah(void) {
+    
+    // test stuff
+  }
+
+int runUnityTests(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_function_should_doBlahAndBlah);
+    return UNITY_END();
+}
+
+/**
+ * For native dev-platform or for some embedded frameworks
+ */
+int main(void)
+{
+    return runUnityTests();
+}
