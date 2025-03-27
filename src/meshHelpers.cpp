d340182cbbefe765b0f6da281694647bc3fbd823
lthyge <laust.thygesen@gmail.com>
2025-03-27 08:58:21 +0100

    Finished test setup

--- src/meshHelpers.cpp
+++ src/meshHelpers.cpp
@@ -6,28 +6,3 @@ void changeAP(painlessMesh &mesh, String ssid)
     mesh.closeConnectionSTA();
     mesh.stationManual(ssid, MESH_PASSWORD, MESH_PORT);
 }
-
-int getRootNodeId(TSTRING connections)
-{
-    const size_t ROOT_NODE_ID_LENGTH = 10; // The length of the root node ID
-    const size_t ROOT_NODE_ID_OFFSET = 13; // Offset from "root" to the start of the ID
-    std::string connectionsStr = connections.c_str();
-    // Find the position of "root"
-    size_t rootStringStartPosition = connectionsStr.find("root");
-    if (rootStringStartPosition == std::string::npos)
-    {
-        return -1; 
-    }
-
-    // Extract the root node ID (assuming a fixed format)
-    size_t idStartPos = rootStringStartPosition - ROOT_NODE_ID_OFFSET;
-    if (idStartPos > connectionsStr.size())
-    {
-        return -2;
-    }
-
-    std::string rootNodeIdStr = connectionsStr.substr(idStartPos, ROOT_NODE_ID_LENGTH);
-    uint32_t rootNodeId = static_cast<uint32_t>(std::stoul(rootNodeIdStr));
-
-    return rootNodeId;
-}
--- test/test_get_root_node_id.cpp
+++ test/test_get_root_node_id.cpp
@@ -1,5 +1,6 @@
 #include "unity.h"
-#include "..\src\meshHelpers.cpp"
+#include "..\lib\painlessMesh\src\painlessmesh\mesh.hpp"
+#include "painlessmesh.h"
 
 void setUp(void)
 {
@@ -13,7 +14,7 @@ void tearDown(void)
 
 void test_getRootNodeId_should_return_3206773453(void)
 {
-    u_int32_t rootId = getRootNodeId("{\"nodeId\" : 3211386233, \"subs\" : [ {\"nodeId\" : 3211408993, \"subs\" : [ {\"nodeId\" : 3206773453, \"root\" : true, \"subs\" : [ {\"nodeId\" : 3206793885} ]} ]} ]}");
+    u_int32_t rootId = painlessMesh::Mesh::getRootNodeId("{\"nodeId\" : 3211386233, \"subs\" : [ {\"nodeId\" : 3211408993, \"subs\" : [ {\"nodeId\" : 3206773453, \"root\" : true, \"subs\" : [ {\"nodeId\" : 3206793885} ]} ]} ]}");
     TEST_ASSERT_EQUAL_INT32(3206773453, rootId);
 }
 
