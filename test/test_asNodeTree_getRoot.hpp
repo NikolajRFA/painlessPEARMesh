49e51666dd0a814abe432381aa4328f72d04765e
NikolajRFA <34486013+NikolajRFA@users.noreply.github.com>
2025-04-01 23:38:02 +0200

    Added getRootNodeId in layout with NodeTree parameter and tests

--- test/test_asNodeTree_getRoot.hpp
+++
@@ -1,9 +0,0 @@
-#include "unity.h"
-#include "../lib/painlessMesh/src/painlessmesh/protocol.hpp"
-#include "../lib/painlessMesh/src/painlessmesh/layout.hpp"
-
-void test_getRootNodeFromNodeTree_should_return_correctRoot(void)
-{
-    auto nt = painlessmesh::protocol::NodeTree(123, false);
-    
-}
--- test/test_runner.cpp
+++ test/test_runner.cpp
@@ -3,6 +3,8 @@
 #include "compareWiFiAPRecords_tests.hpp"
 #include "test_get_root_node_id.hpp"
 #include "test_containsTargetBSSID.hpp"
+#include "test_jsonHelper.hpp"
+#include "getRootNodeIdTests.hpp"
 
 void setUp(void)
 {
@@ -22,7 +24,12 @@ int runUnityTests(void)
     RUN_TEST(test_compareWiFiAPRecords_where_2nd_has_better_rssi_should_return_false);
     RUN_TEST(test_compareWiFiAPRecords_through_ap_list_and_with_useTargetBSSID_then_target_BSSID_is_first);
     RUN_TEST(test_containsTargetBSSID_should_return_true);
-    RUN_TEST(test_getRootNodeFromNodeTree_should_return_correctRoot);
+    RUN_TEST(test_build_new_parent_json_should_return_jsonString);
+    RUN_TEST(test_json_contains_new_parent_returns_true);
+    RUN_TEST(test_json_contains_new_parent_returns_false);
+    RUN_TEST(test_buildNewParentJson_and_jsonContainsNewParent_returns_true);
+    RUN_TEST(getRootNodeId_NodeTreeWithRootNode_1);
+    RUN_TEST(getRootNodeId_NodeTreeWithoutRootNode_0);
     return UNITY_END();
 }
 
