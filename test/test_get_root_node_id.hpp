a0e0e47ee9fd2b887f13f889c4152d1cf198375c
lthyge <laust.thygesen@gmail.com>
2025-04-02 09:48:45 +0200

    Removed legacy getRootNodeId

--- test/test_get_root_node_id.hpp
+++
@@ -1,9 +0,0 @@
-#include "unity.h"
-#include "..\lib\painlessMesh\src\painlessmesh\mesh.hpp"
-#include "painlessmesh.h"
-
-void test_getRootNodeId_should_return_3206773453(void)
-{
-    u_int32_t rootId = painlessMesh::Mesh::getRootNodeId("{\"nodeId\" : 3211386233, \"subs\" : [ {\"nodeId\" : 3211408993, \"subs\" : [ {\"nodeId\" : 3206773453, \"root\" : true, \"subs\" : [ {\"nodeId\" : 3206793885} ]} ]} ]}");
-    TEST_ASSERT_EQUAL_INT32(3206773453, rootId);
-}
--- test/test_runner.cpp
+++ test/test_runner.cpp
@@ -1,7 +1,6 @@
 #include "unity.h"
 #include "painlessMeshSTA.h"
 #include "compareWiFiAPRecords_tests.hpp"
-#include "test_get_root_node_id.hpp"
 #include "test_containsTargetBSSID.hpp"
 #include "test_jsonHelper.hpp"
 #include "getRootNodeIdTests.hpp"
@@ -19,7 +18,6 @@ void tearDown(void)
 int runUnityTests(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_getRootNodeId_should_return_3206773453);
     RUN_TEST(test_compareWiFiAPRecords_with_useTargetBSSID_should_return_true);
     RUN_TEST(test_compareWiFiAPRecords_where_2nd_has_better_rssi_should_return_false);
     RUN_TEST(test_compareWiFiAPRecords_through_ap_list_and_with_useTargetBSSID_then_target_BSSID_is_first);
