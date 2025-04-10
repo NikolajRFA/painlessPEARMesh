4af52a794e24a8e5aa49a3b78b27d2b8183954b7
lthyge <laust.thygesen@gmail.com>
2025-04-10 12:36:29 +0200

    Added PearNodeTree actually

--- lib/painlessMesh/src/painlessmesh/pear.hpp
+++ lib/painlessMesh/src/painlessmesh/pear.hpp
@@ -2,6 +2,9 @@
 #define _PAINLESS_MESH_PEAR_HPP_
 
 #include <Arduino.h>
+#include <set>
+#include "mesh.hpp"
+#include "protocol.hpp"
 
 namespace painlessmesh {
   class PearNodeTree;
@@ -35,6 +38,36 @@ namespace painlessmesh {
     // if visibleNode is within limits set nodeToReroute.newParent = visibleNode
   }
 };
+
+  class PearNodeTree : public protocol::NodeTree {
+  public:
+    int periodTx = 0;
+    int periodRx = 0;
+    std::list<uint32_t> parentCandidates;
+    int energyProfile = 200;
+    std::set<PearNodeTree> subsDescendingTx;
+
+    // Define the < operator for comparison
+    bool operator<(const PearNodeTree& other) const {
+      return periodRx > other.periodRx;
+    }
+
+    PearNodeTree(){}
+
+    PearNodeTree(const NodeTree &nodeTree, const int periodTx, const int periodRx, const std::list<uint32_t> parentCandidates) {
+      this->nodeId = nodeTree.nodeId;
+      this->root = nodeTree.root;
+      this->subs = nodeTree.subs;
+      this->periodRx = periodRx;
+      this->periodTx = periodTx;
+      this->parentCandidates = parentCandidates;
+      this->subsDescendingTx.insert(subs.begin(), subs.end());
+    }
+
+  private:
+  };
+
+
 
 }
 
