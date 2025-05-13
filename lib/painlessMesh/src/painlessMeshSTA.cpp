d6de03f812ce2d22033cbe46f6e4316d23587bc6
lthyge <laust.thygesen@gmail.com>
2025-05-13 11:15:43 +0200

    Added removal of station

--- lib/painlessMesh/src/painlessMeshSTA.cpp
+++ lib/painlessMesh/src/painlessMeshSTA.cpp
@@ -42,6 +42,7 @@ void ICACHE_FLASH_ATTR StationScan::stationScan() {
   using namespace painlessmesh::logger;
   Log(CONNECTION, "stationScan(): %s\n", ssid.c_str());
   if (checkStation()) {
+    removeStationFromAvailableNetworksIfInNodeSubs();
     Log(PEAR, "Already connected to target - cancelling scan\n");
     return;
   }
