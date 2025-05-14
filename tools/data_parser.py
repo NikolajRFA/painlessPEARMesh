17468bc880b59d99adf0b034ce5c29e2fd2df8eb
NikolajRFA <nrfadk@gmail.com>
2025-05-14 10:34:08 +0200

    Added def for csv_from_json function

--- tools/data_parser.py
+++ tools/data_parser.py
@@ -10,4 +10,7 @@ def extract_data_jsons(file_path):
                 data_jsons.append(json.loads(data_json))
     return data_jsons
 
+def csv_from_json(json_data):
+
+
 print(extract_data_jsons('20_2_1305_1.txt'))
