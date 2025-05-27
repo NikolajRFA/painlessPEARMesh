4fa4bccbffb4ccd66e9bee5a56b13993242ada2a
NikolajRFA <nrfadk@gmail.com>
2025-05-27 15:50:06 +0200

    Latest

--- tools/char_count.py
+++ tools/char_count.py
@@ -1,7 +1,7 @@
 import pdfplumber
 
 total_chars = 0
-with pdfplumber.open("data/Master_Thesis_S2025-6.pdf") as pdf:
+with pdfplumber.open("data/Master_Thesis_S2025-10.pdf") as pdf:
     for page in pdf.pages:
         text = page.extract_text()
         if text:
